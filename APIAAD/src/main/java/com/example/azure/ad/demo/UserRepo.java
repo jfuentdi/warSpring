9c7bffaa2d5c069cc16d7026dcbe6b081360dd6e
jfuentdi <jfuentdi@emeal.nttdata.com>
2022-12-13 14:28:04 -0300

    aa

--- APIAAD/src/main/java/com/example/azure/ad/demo/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.azure.ad.demo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepo extends JpaRepository<Users, Integer> {
-    
-}
