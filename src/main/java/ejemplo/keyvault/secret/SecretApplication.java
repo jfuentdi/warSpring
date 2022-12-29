62854682825c42b334434d60fcab0750d183c56f
jfuentdi <jfuentdi@emeal.nttdata.com>
2022-12-29 10:52:58 -0300

    test

---
+++ src/main/java/ejemplo/keyvault/secret/SecretApplication.java
@@ -0,0 +1,16 @@
+package ejemplo.keyvault.secret;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SecretApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecretApplication.class, args);
+	}
+
+
+}
